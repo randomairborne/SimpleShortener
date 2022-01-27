36fb4eeab265b51c26b489f7e8ff60b83a81f83f
valkyrie_pilot <valk@randomairborne.dev>
2022-01-26 19:04:46 -0700

    update package script

--- packaging/package.sh
+++ packaging/package.sh
@@ -2,6 +2,8 @@
 SQLX_OFFLINE=true
 cargo build --release --target x86_64-unknown-linux-gnu
 cross build --release --target aarch64-unknown-linux-gnu
+mkdir -p ./simpleshortener_*_amd64/usr/sbin/
+mkdir -p ./simpleshortener_*_arm64/usr/sbin/
 mv ../target/x86_64-unknown-linux-gnu/release/simple_shortener ./simpleshortener_*_amd64/usr/sbin/
 mv ../target/aarch64-unknown-linux-gnu/release/simple_shortener ./simpleshortener_*_arm64/usr/sbin/
 dpkg-deb --build --root-owner-group ./simpleshortener_*_amd64
