54c92bb21ebb28eb4cacb2c95de178719aeb01bd
Grant Hutchinson <valk@randomairborne.dev>
2022-01-26 20:24:44 -0700

    more deb fixes

--- packaging/package.sh
+++ packaging/package.sh
@@ -2,9 +2,9 @@
 SQLX_OFFLINE=true
 cargo build --release --target x86_64-unknown-linux-gnu
 cross build --release --target aarch64-unknown-linux-gnu
-mkdir -p ./simpleshortener_*_amd64/usr/sbin/
-mkdir -p ./simpleshortener_*_arm64/usr/sbin/
-mv ../target/x86_64-unknown-linux-gnu/release/simple_shortener ./simpleshortener_*_amd64/usr/sbin/
-mv ../target/aarch64-unknown-linux-gnu/release/simple_shortener ./simpleshortener_*_arm64/usr/sbin/
+mv ../target/x86_64-unknown-linux-gnu/release/simple_shortener ./simpleshortener_*_amd64/usr/bin/
+mv ../target/aarch64-unknown-linux-gnu/release/simple_shortener ./simpleshortener_*_arm64/usr/bin/
+rm ./simpleshortener_*_amd64/usr/bin/README
+rm ./simpleshortener_*_arm64/usr/bin/README
 dpkg-deb --build --root-owner-group ./simpleshortener_*_amd64
 dpkg-deb --build --root-owner-group ./simpleshortener_*_arm64
