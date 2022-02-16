3876b2e98a4a041572b92b77c5186e09ff62dfdd
valkyrie_pilot <valk@randomairborne.dev>
2022-02-15 22:32:16 -0700

    Update redirect_handler.rs

--- src/redirect_handler.rs
+++ src/redirect_handler.rs
@@ -19,3 +19,38 @@ pub async fn redirect(
     headers.insert(axum::http::header::LOCATION, destination);
     Ok((axum::http::StatusCode::PERMANENT_REDIRECT, headers))
 }
+
+
+// Checks if a specific config var exists or serves the default root
+pub async fn root() -> Result<
+    (axum::http::StatusCode, axum::http::HeaderMap, &'static str),
+    crate::structs::WebServerError,
+> {
+    tracing::trace!("Handling root request");
+    let config = match crate::CONFIG.get() {
+        None => return Err(crate::structs::WebServerError::ConfigNotFound),
+        Some(config) => config.clone(),
+    };
+    let mut headers = axum::http::HeaderMap::new();
+    match config.root {
+        None => {
+            headers.insert(
+                axum::http::header::CONTENT_TYPE,
+                axum::http::HeaderValue::from_static("text/html"),
+            );
+            Ok((
+                axum::http::StatusCode::OK,
+                headers,
+                include_str!("resources/root.html"),
+            ))
+        }
+        Some(root) => {
+            let destination = match axum::http::HeaderValue::from_str(root.as_str()) {
+                Ok(dest) => dest,
+                Err(_) => return Err(crate::structs::WebServerError::InvalidRedirectUri),
+            };
+            headers.insert(axum::http::header::LOCATION, destination);
+            Ok((axum::http::StatusCode::PERMANENT_REDIRECT, headers, ""))
+        }
+    }
+}
