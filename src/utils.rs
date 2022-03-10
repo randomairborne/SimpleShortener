5a514e352ec2113b0dbe7e8f6273afe512de9e47
0/0 <zero@imaskeleton.me>
2022-03-09 21:49:37 -0700

    fix CLion complaints

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,7 @@ use std::fs::{self, File, OpenOptions};
 use std::io::{self, Read, Write};
 use std::path::Path;
 
-pub fn read_bincode(filename: &String) -> dashmap::DashMap<String, String> {
+pub fn read_bincode(filename: &String) -> DashMap<String, String> {
     tracing::debug!("Reading bincode database file!");
     let mut f = OpenOptions::new()
         .write(true)
@@ -15,19 +15,19 @@ pub fn read_bincode(filename: &String) -> dashmap::DashMap<String, String> {
     let metadata = fs::metadata(&filename).expect("unable to read metadata");
     let mut buffer = vec![0; metadata.len() as usize];
     f.read_exact(&mut buffer).expect("buffer overflow");
-    let deserialized: dashmap::DashMap<String, String> = match bincode::deserialize(&buffer[..]) {
-        Ok(dashmap) => dashmap,
+    let deserialized: DashMap<String, String> = match bincode::deserialize(&buffer[..]) {
+        Ok(map) => map,
         Err(e) => {
             match *e {
-                bincode::ErrorKind::Io(ioerror) => {
-                    if ioerror.kind() == io::ErrorKind::UnexpectedEof {
+                bincode::ErrorKind::Io(io_error) => {
+                    if io_error.kind() == io::ErrorKind::UnexpectedEof {
                         tracing::warn!("Creating new database...");
-                        let new_dashmap: dashmap::DashMap<String, String> = dashmap::DashMap::new();
-                        let encoded: Vec<u8> = bincode::serialize(&new_dashmap).unwrap();
+                        let new_map: DashMap<String, String> = DashMap::new();
+                        let encoded: Vec<u8> = bincode::serialize(&new_map).unwrap();
                         f.write_all(&encoded).expect("Failed to write file!?");
-                        return dashmap::DashMap::new();
+                        return DashMap::new();
                     }
-                    panic!("I/O Error: {}", ioerror)
+                    panic!("I/O Error: {}", io_error)
                 }
                 _ => panic!("Error deserializing database: {}", e),
             };
