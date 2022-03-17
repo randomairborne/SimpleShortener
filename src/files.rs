3ea80c66bf980b1e9405b005e7268f926e1b5c1c
valkyrie_pilot <valk@randomairborne.dev>
2022-03-17 20:33:22 +0000

    Added live HTML reloading in debug mode

--- src/files.rs
+++ src/files.rs
@@ -2,23 +2,32 @@ use axum::http::header::CONTENT_TYPE;
 use axum::http::{HeaderMap, HeaderValue, StatusCode};
 use tracing::debug;
 
+
 // basic handler that responds with a static string
-pub async fn doc() -> (StatusCode, HeaderMap, &'static str) {
+pub async fn doc() -> (StatusCode, HeaderMap, String) {
     let mut headers = HeaderMap::new();
     headers.insert(CONTENT_TYPE, HeaderValue::from_static("text/html"));
     debug!("Handling doc request");
-    (StatusCode::OK, headers, include_str!("resources/doc.html"))
+    #[cfg(not(debug_assertions))]
+    let docs = include_str!("resources/doc.html").to_string();
+    #[cfg(debug_assertions)]
+    let docs = std::fs::read_to_string("src/resources/doc.html").expect("Program is in debug mode and the documentation file was not found!");
+    (StatusCode::OK, headers, docs)
 }
 
 // basic handler that responds with a static string
-pub async fn panel_html() -> (StatusCode, HeaderMap, &'static str) {
+pub async fn panel_html() -> (StatusCode, HeaderMap, String) {
     let mut headers = HeaderMap::new();
     headers.insert(CONTENT_TYPE, HeaderValue::from_static("text/html"));
     debug!("Handling html request");
+    #[cfg(not(debug_assertions))]
+    let panel = include_str!("resources/panel.html").to_string();
+    #[cfg(debug_assertions)]
+    let panel = std::fs::read_to_string("src/resources/panel.html").expect("Program is in debug mode and the panel file was not found!");
     (
         StatusCode::OK,
         headers,
-        include_str!("resources/panel.html"),
+        panel,
     )
 }
 
