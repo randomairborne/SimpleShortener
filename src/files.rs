6b7588a7b4e0a8133539f41b772dfffcb9859259
valkyrie_pilot <valk@randomairborne.dev>
2022-03-18 02:53:17 +0000

    Remove built-in fonts in favor of Google ones

--- src/files.rs
+++ src/files.rs
@@ -31,30 +31,6 @@ pub async fn panel_html() -> (StatusCode, HeaderMap, String) {
     )
 }
 
-// basic handler that responds with a static font file
-pub async fn font2() -> (StatusCode, HeaderMap, &'static [u8]) {
-    let mut headers = HeaderMap::new();
-    headers.insert(CONTENT_TYPE, HeaderValue::from_static("font/woff2"));
-    debug!("Handling woff2 font request");
-    (
-        StatusCode::OK,
-        headers,
-        include_bytes!("resources/font.woff2").as_ref(),
-    )
-}
-
-// basic handler that responds with a static font file
-pub async fn font() -> (StatusCode, HeaderMap, &'static [u8]) {
-    let mut headers = HeaderMap::new();
-    headers.insert(CONTENT_TYPE, HeaderValue::from_static("font/woff"));
-    debug!("Handling woff font request");
-    (
-        StatusCode::OK,
-        headers,
-        include_bytes!("resources/font.woff").as_ref(),
-    )
-}
-
 // basic handler that responds with a static icon file
 pub async fn favicon() -> (StatusCode, HeaderMap, &'static [u8]) {
     let mut headers = HeaderMap::new();
